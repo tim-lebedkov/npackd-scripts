6afcdadb8242082fdf4d9ffb2eb0f4e411698a31
Tim Lebedkov <tim.lebedkov@gmail.com>
2015-05-13 21:06:08 +0200

    git instead of hg

--- Transfer.bat
+++ Transfer.bat
@@ -1,22 +1,21 @@
 rem This script transfers the 5 default repositories from 
 rem http://npackd.appspot.com
-rem to the Mercurial source repository at
-rem http://code.google.com/p/windows-package-manager so that the repositories
-rem are available e.g. under 
-rem https://windows-package-manager.googlecode.com/hg/repository/Rep.xml
+rem to the Git source repository at
+rem https://github.com/tim-lebedkov/npackd
+rem
 rem The 5 repositories are:
 rem http://npackd.appspot.com/rep/xml?tag=stable
 rem http://npackd.appspot.com/rep/xml?tag=stable64
 rem http://npackd.appspot.com/rep/xml?tag=libs 
 rem http://npackd.appspot.com/rep/xml?tag=unstable
 rem http://npackd.appspot.com/rep/xml?tag=vim
-rem You would require commit permission for the Mercurial repository.
+rem You would require commit permission for the Git repository.
 
 set onecmd="%npackd_cl%\npackdcl.exe" path --package=org.xmlsoft.LibXML --versions=[2.4.12,2.4.12]
 for /f "usebackq delims=" %%x in (`%%onecmd%%`) do set libxml=%%x
 
-set onecmd="%npackd_cl%\npackdcl.exe" path --package=com.selenic.mercurial.Mercurial64 --versions=[1.8,3)
-for /f "usebackq delims=" %%x in (`%%onecmd%%`) do set mercurial=%%x
+set onecmd="%npackd_cl%\npackdcl.exe" path --package=com.googlecode.msysgit.MSysGit --versions=[1.7,2)
+for /f "usebackq delims=" %%x in (`%%onecmd%%`) do set git=%%x
 
 set onecmd="%npackd_cl%\npackdcl.exe" path --package=se.haxx.curl.CURL64 --versions=[7,10)
 for /f "usebackq delims=" %%x in (`%%onecmd%%`) do set curl=%%x
@@ -30,11 +29,11 @@ set d=%temp%\%random%
 mkdir %d%
 if %errorlevel% neq 0 goto error
 
-set /p user=Please enter your code.google.com user name (e.g. black.smith): 
+set /p user=Please enter your Github.com user name (e.g. black.smith): 
 if "%user%" equ "" pause & goto :eof
 if %errorlevel% neq 0 goto error
 
-"%mercurial%\hg.exe" clone https://%user%@code.google.com/p/windows-package-manager/ "%d%"
+"%git%\bin\git.exe" clone https://github.com/tim-lebedkov/npackd.git "%d%"
 if %errorlevel% neq 0 goto error
 
 "%curl%\bin\curl.exe" -o "%d%\repository\Rep.xml" http://npackd.appspot.com/rep/xml?tag=stable
@@ -67,11 +66,15 @@ if %errorlevel% neq 0 goto error
 "%libxml%\bin\xmllint.exe" --noout "%d%\repository\Vim.xml"
 if %errorlevel% neq 0 goto error
 
-"%mercurial%\hg.exe" commit -R "%d%"
+pushd %d%
+
+"%git%\bin\git.exe" commit -a -m "Updates from npackd.appspot.com"
 if %errorlevel% neq 0 goto error
 
-"%mercurial%\hg.exe" push -R "%d%"
+"%git%\bin\git.exe" push
 if %errorlevel% neq 0 goto error
+
+popd
 
 rem removing the temporary directory
 if "%d%" neq "" rmdir /s /q "%d%"
