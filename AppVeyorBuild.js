243e934b439f51738939322cbb1a953e596e6f05
Tim Lebedkov <tim.lebedkov@gmail.com>
2017-06-25 12:13:56 +0200

    quotes

--- AppVeyorBuild.js
+++ AppVeyorBuild.js
@@ -169,7 +169,7 @@ function process() {
                 "C:\\NpackdSymlinks\\" +
                 "com.nokia.QtDev-i686-w64-Npackd-Release-5.5\\qtbase\\" +
                 "bin\\qmake.exe " +
-                "\"LIBS+=-L" + libz + "\\lib\" \"INCLUDEPATH+=" + libz + 
+                "LIBS+=\"-L" + libz + "\\lib\" INCLUDEPATH+=\"" + libz + 
                 "\\include\" " +
                 "CONFIG+=staticlib CONFIG+=release DEFINES+=QUAZIP_STATIC");
         execSafe("set path=" + mingw + 
