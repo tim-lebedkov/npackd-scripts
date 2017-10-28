554a3cf1e2ce7165625efec5ec47b12502ac4bca
tim-lebedkov <tim.lebedkov@gmail.com>
2017-10-28 20:41:43 +0200

    use NpackdCL 1.23

--- AppVeyorInstall.bat
+++ AppVeyorInstall.bat
@@ -3,23 +3,23 @@ echo on
 rem This script is used by AppVeyor automatic builds to install the necessary
 rem software dependencies.
 
-msiexec.exe /qn /i https://github.com/tim-lebedkov/npackd-cpp/releases/download/version_1.21.6/NpackdCL-1.21.6.msi
+msiexec.exe /qn /i https://github.com/tim-lebedkov/npackd-cpp/releases/download/version_1.23/NpackdCL-1.23.msi
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-SET NPACKD_CL=C:\Program Files (x86)\NpackdCL
+set path=C:\ProgramData\Npackd\Commands;C:\Program Files (x86)\NpackdCL;C:\Windows\System32
 
-"%npackd_cl%\ncl" add-repo --url=https://npackd.appspot.com/rep/recent-xml
+ncl add-repo --url=https://npackd.appspot.com/rep/recent-xml
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-"%npackd_cl%\ncl" add-repo --url=https://npackd.appspot.com/rep/xml?tag=libs
+ncl add-repo --url=https://npackd.appspot.com/rep/xml?tag=libs
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-"%npackd_cl%\ncl" detect
+ncl detect
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-"%npackd_cl%\ncl" help
+ncl help
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-"%npackd_cl%\ncl" set-install-dir -f "C:\Program Files (x86)"
+ncl set-install-dir -f "C:\Program Files (x86)"
 if %errorlevel% neq 0 exit /b %errorlevel%
 
