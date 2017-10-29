191e567a211fce3d45622cbeb2e5113dadb35f3f
tim-lebedkov <tim.lebedkov@gmail.com>
2017-10-29 10:31:55 +0100

    find appveyor.bat

--- AppVeyorBuild.bat
+++ AppVeyorBuild.bat
@@ -2,6 +2,8 @@ echo on
 
 rem This script is used by AppVeyor to build the project.
 
+where appveyor
+
 set path=C:\ProgramData\Npackd\Commands;C:\Program Files (x86)\NpackdCL;C:\Windows\System32
 
 cscript AppVeyorBuild.js
