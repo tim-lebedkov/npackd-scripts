583f5e108505684d6fe3975311226017e2218997
tim-lebedkov <tim.lebedkov@gmail.com>
2017-10-29 10:34:07 +0100

    put appveyor in the PATH

--- AppVeyorBuild.bat
+++ AppVeyorBuild.bat
@@ -4,7 +4,7 @@ rem This script is used by AppVeyor to build the project.
 
 where appveyor
 
-set path=C:\ProgramData\Npackd\Commands;C:\Program Files (x86)\NpackdCL;C:\Windows\System32
+set path=C:\ProgramData\Npackd\Commands;C:\Program Files (x86)\NpackdCL;C:\Program Files\AppVeyor\BuildAgent;C:\Windows\System32
 
 cscript AppVeyorBuild.js
 
